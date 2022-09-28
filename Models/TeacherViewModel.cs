1c9d70289ae637d2b1ccdb35a81ddf03963d33cd
Behzod <behzodsafarov73@gmail.com>
2022-09-28 21:26:41 +0500

    Problem With mapping

--- Models/TeacherViewModel.cs
+++ Models/TeacherViewModel.cs
@@ -5,13 +5,10 @@ public class TeacherViewModel
 {
  [Required]
  public int Age { get; set; }
-
  [Required,MaxLength(100)]
  public string? Adress { get; set; }
-
  [Required]
  public  List<CoureViewModel>? CourseNames { get; set; }
-
  public long  Salary { get; set; }   
  public EPatogs Patogs { get; set; }
 }
